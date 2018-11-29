cac374ba48c1ccda6406512833df1b9da55173b5
Anthony Ofume <praiseofume@gmail.com>
2018-11-29 16:18:18 +0100

    Created the Api and filled in the create section into an array

---
+++ ourBlog/routes/blog.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var router = express.Router();
+let blogArray = [];
+let id = 1;
+/* GET users listing. */
+router.get("/", function(req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.post("/create", function(req, res, next) {
+  let body = req.body;
+  body.id = id;
+  id += 1;
+  blogArray.push(body);
+  console.log(blogArray);
+  res.send("Array saved");
+});
+router.get("/read", function(req, res, next) {
+  res.send("You are reading");
+});
+router.put("/update", function(req, res, next) {
+  res.send("You are updating");
+});
+router.delete("/delete", function(req, res, next) {
+  res.send("You are deleting");
+});
+
+module.exports = router;
